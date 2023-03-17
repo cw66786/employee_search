8493b7f2b561f410d0d81410686eec509cffd5dd
Craig Wilson <cw66786@gmail.com>
2023-03-17 13:41:25 -0500

    service created and list function done

--- src/app/core/employee.service.ts
+++ src/app/core/employee.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private baseUrl: string = 'https://regres.in/api/'
+  private page2: string = '?page=2'
 
-  constructor() { }
+  employeeList: string[]= [];
+  employeeListBehave = new BehaviorSubject(this.employeeList);
+  employeeList$ = this.employeeListBehave.asObservable();
+
+
+  constructor(private http: HttpClient) { }
+
+
+  getEmployeeList(){
+    return this.http.get(this.baseUrl + this.page2), tap((employees: string[])=>{
+      this.employeeList = employees;
+      this.employeeListBehave.next(this.employeeList);
+    })
+
+  }
+
+
+getSingleEmployee(userInput: string){
+  return 'works';
+}
+
+
 }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, filter, fromEvent, Subscription, switchMap } from 'rxjs';
+import { EmployeeService } from 'src/app/core/employee.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  @ViewChild('searchBox', {static: true}) input!: ElementRef;
 
-  constructor() { }
+  private eventSubscription!: Subscription;
+
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.eventSubscription = fromEvent(this.input.nativeElement, 'keyup')
+    .pipe(
+     debounceTime(500),
+     filter((_)=>{
+       return this.input.nativeElement.value.trim() !== '';
+     }),
+     switchMap((_)=>{
+       return this.employeeService.getSingleEmployee(
+         this.input.nativeElement.value
+       );
+     })
+    )
+    .subscribe();
   }
+
+
+  ngOnDestroy(): void {
+    this.eventSubscription.unsubscribe();
+  }
+
 
 }
