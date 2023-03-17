c538afcaa25dab498136f19316035ca9a51f3adf
Craig Wilson <cw66786@gmail.com>
2023-03-17 13:12:09 -0500

    components created .. general setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
